154430201d70796c184cd142bf684f527e583088
cotto89 <cottto89@gmail.com>
2017-01-10 20:35:36 +0900

    Add reaction()
    close #2
    close #5

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-export type Subscriber<T> = {[P in keyof T]?: (payload: T[P]) => any};
-
 export interface Dispatch<T> {
     <K extends keyof T>(event: K, payload: T[K]): void;
 }
+
+export type Subscriber<T> = {[P in keyof T]?: (payload: T[P]) => any};
 
 export interface Subscribe<T> {
     (subscriber: Subscriber<T>): Function;
@@ -12,10 +12,15 @@ export interface SubscribeAll<T> {
     <K extends keyof T>(subscriber: (event: K, payload: T[K]) => any): Function;
 }
 
-export type Events<T> = Map<keyof T, ((payload: any) => any)[]>;
+export type Reactions<T> = {[P in keyof T]?: (payload: T[P]) => Partial<T> | Promise<Partial<T>>};
+
+export interface Reaction<T> {
+    (reactions: Reactions<T>): Function;
+}
 
 export class Dispatcher<T> {
-    readonly events: Events<T> = new Map();
+    readonly events: Map<keyof T, ((payload: any) => any)[]> = new Map();
+    readonly reacts: Map<keyof T, ((payload: any) => Partial<T>)[]> = new Map();
     readonly listeners: Function[] = [];
 
     /**
@@ -24,13 +29,28 @@ export class Dispatcher<T> {
      * @param {string} event
      * @param {EventTypes[K]} payload
      */
+    /* tslint:disable:no-unused-expression */
     dispatch<K extends keyof T>(event: K, payload: T[K]) {
-        const subscriber = this.events.get(event);
-
+        // publish for subscribeAll
         this.listeners.forEach((f) => f(event, payload));
 
-        if (!subscriber) return;
-        subscriber.forEach((f) => f(payload));
+        // publish for subscriber
+        const subscriber = this.events.get(event);
+        subscriber && subscriber.forEach((f) => f(payload));
+
+        // invoke reaction
+        const react = this.reacts.get(event);
+        react && react.forEach((f) => {
+            const ret = f(payload);
+
+            if (ret instanceof Promise) {
+                Promise.resolve(ret).then((r) => {
+                    this._dispatchFromObject(r);
+                });
+            } else {
+                this._dispatchFromObject(ret);
+            }
+        });
     }
 
 
@@ -80,6 +100,34 @@ export class Dispatcher<T> {
         };
     }
 
+
+    /**
+     * Reaction(Subscribe event for reaction)
+     * Reaction is subscribe event -> publish other event
+     */
+    reaction(reactions: Reactions<T>): Function {
+        Object.keys(reactions).forEach((k: keyof T) => {
+            if (!this.reacts.has(k)) {
+                this.reacts.set(k, []);
+            }
+
+            (this.reacts.get(k) as Function[]).push(reactions[k] as Function);
+        });
+
+        /* dispose */
+        return () => {
+            Object.keys(reactions).forEach((k: keyof T) => {
+                const reacts = this.reacts.get(k);
+
+                if (!reacts) return;
+
+                const idx = reacts.findIndex(t => t === reactions[k]);
+                reacts.splice(idx, 1);
+            });
+        };
+    }
+
+
     get eventCount() {
         return this.events.size;
     }
@@ -87,6 +135,14 @@ export class Dispatcher<T> {
     getSubscribers(event: keyof T) {
         return [...this.events.get(event) as Function[]];
     }
+
+
+    private _dispatchFromObject(obj: any) {
+        Object.keys(obj).forEach((k: any) => {
+            this.dispatch(k, obj[k]);
+        });
+    };
+
 }
 
 export default Dispatcher;
--- test/index.test.ts
+++ test/index.test.ts
@@ -2,12 +2,12 @@ import * as assert from 'power-assert';
 import * as sinon from 'sinon';
 import Dispatcher, { Dispatch } from './../src/index';
 
-interface EventTypes {
-    action: number;
-    action2: { count: number };
-}
-
 describe('Dispatcher', () => {
+    interface EventTypes {
+        action: number;
+        action2: { count: number };
+    }
+
     let dispatcher: Dispatcher<EventTypes>;
     let dispatch: Dispatch<EventTypes>;
     const spy1 = sinon.spy();
@@ -59,5 +59,65 @@ describe('Dispatcher', () => {
 
         unsubscribe();
         assert.equal(dispatcher.listeners.length, 0);
+    });
+
+    it('reaction()', () => {
+        dispatcher.subscribeAll(spy1);
+
+        const spy3 = sinon.spy();
+        const spy4 = sinon.spy((payload) => ({
+            action2: { count: payload + 10 }
+        }));
+
+        dispatcher.subscribe({
+            action: spy2,
+            action2: spy3
+        });
+
+        const dispose = dispatcher.reaction({
+            action: spy4
+        });
+
+        assert.equal(dispatcher.reacts.get('action').length, 1);
+
+        dispatcher.dispatch('action', 10);
+
+        // subscribeAll
+        assert(spy1.firstCall.calledWithExactly('action', 10));
+        assert(spy1.secondCall.calledWithExactly('action2', { count: 20 }));
+
+        // subscribers
+        assert(spy2.calledWithExactly(10));
+        assert(spy3.calledWithExactly({ count: 20 }));
+
+        // reaction
+        assert(spy4.calledWithExactly(10));
+
+        // dispose
+        dispose();
+        assert.equal(dispatcher.reacts.get('action').length, 0);
+    });
+
+
+    it('react() with Promise', async () => {
+        dispatcher.subscribeAll((spy1));
+
+        const spy4 = sinon.spy((payload) => Promise.resolve({
+            action2: { count: payload + 10 }
+        }));
+
+        dispatcher.reaction({
+            action: spy4
+        });
+
+        dispatcher.dispatch('action', 10);
+
+        await Promise.resolve().then(() => {
+            assert(spy1.firstCall.calledWithExactly('action', 10));
+            assert(spy1.secondCall.calledWithExactly('action2', { count: 20 }));
+
+            // reaction
+            assert(spy4.calledWithExactly(10));
+        });
     });
 });
