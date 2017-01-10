10994ce6c1360fb0bb47cf16e129a4e2d00dfb94
cotto89 <cottto89@gmail.com>
2017-01-10 20:46:23 +0900

    Publish@0.0.7

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,18 +1,24 @@
-export declare type Subscriber<T> = {
-    [P in keyof T]?: (payload: T[P]) => any;
-};
 export interface Dispatch<T> {
     <K extends keyof T>(event: K, payload: T[K]): void;
 }
+export declare type Subscriber<T> = {
+    [P in keyof T]?: (payload: T[P]) => any;
+};
 export interface Subscribe<T> {
     (subscriber: Subscriber<T>): Function;
 }
 export interface SubscribeAll<T> {
-    <K extends keyof T>(event: K, payload: T[K]): Function;
+    <K extends keyof T>(subscriber: (event: K, payload: T[K]) => any): Function;
 }
-export declare type Events<T> = Map<keyof T, ((payload: any) => any)[]>;
+export declare type Reactions<T> = {
+    [P in keyof T]?: (payload: T[P]) => Partial<T> | Promise<Partial<T>>;
+};
+export interface Reaction<T> {
+    (reactions: Reactions<T>): Function;
+}
 export declare class Dispatcher<T> {
-    readonly events: Events<T>;
+    readonly events: Map<keyof T, ((payload: any) => any)[]>;
+    readonly reacts: Map<keyof T, ((payload: any) => Partial<T>)[]>;
     readonly listeners: Function[];
     /**
      * Dispatch an event
@@ -35,7 +41,13 @@ export declare class Dispatcher<T> {
      * @returns {Function} unsubscribe
      */
     subscribeAll<K extends keyof T>(subscriber: (event: K, payload: T[K]) => any): () => void;
+    /**
+     * Reaction(Subscribe event for reaction)
+     * Reaction is subscribe event -> publish other event
+     */
+    reaction(reactions: Reactions<T>): Function;
     readonly eventCount: number;
     getSubscribers(event: keyof T): Function[];
+    private _dispatchFromObject(obj);
 }
 export default Dispatcher;
--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,7 @@
 class Dispatcher {
     constructor() {
         this.events = new Map();
+        this.reacts = new Map();
         this.listeners = [];
     }
     /**
@@ -10,12 +11,26 @@ class Dispatcher {
      * @param {string} event
      * @param {EventTypes[K]} payload
      */
+    /* tslint:disable:no-unused-expression */
     dispatch(event, payload) {
-        const subscriber = this.events.get(event);
+        // publish for subscribeAll
         this.listeners.forEach((f) => f(event, payload));
-        if (!subscriber)
-            return;
-        subscriber.forEach((f) => f(payload));
+        // publish for subscriber
+        const subscriber = this.events.get(event);
+        subscriber && subscriber.forEach((f) => f(payload));
+        // invoke reaction
+        const react = this.reacts.get(event);
+        react && react.forEach((f) => {
+            const ret = f(payload);
+            if (ret instanceof Promise) {
+                Promise.resolve(ret).then((r) => {
+                    this._dispatchFromObject(r);
+                });
+            }
+            else {
+                this._dispatchFromObject(ret);
+            }
+        });
     }
     /**
      * Subscribe events
@@ -55,12 +70,40 @@ class Dispatcher {
             this.listeners.splice(idx, 1);
         };
     }
+    /**
+     * Reaction(Subscribe event for reaction)
+     * Reaction is subscribe event -> publish other event
+     */
+    reaction(reactions) {
+        Object.keys(reactions).forEach((k) => {
+            if (!this.reacts.has(k)) {
+                this.reacts.set(k, []);
+            }
+            this.reacts.get(k).push(reactions[k]);
+        });
+        /* dispose */
+        return () => {
+            Object.keys(reactions).forEach((k) => {
+                const reacts = this.reacts.get(k);
+                if (!reacts)
+                    return;
+                const idx = reacts.findIndex(t => t === reactions[k]);
+                reacts.splice(idx, 1);
+            });
+        };
+    }
     get eventCount() {
         return this.events.size;
     }
     getSubscribers(event) {
         return [...this.events.get(event)];
     }
+    _dispatchFromObject(obj) {
+        Object.keys(obj).forEach((k) => {
+            this.dispatch(k, obj[k]);
+        });
+    }
+    ;
 }
 exports.Dispatcher = Dispatcher;
 Object.defineProperty(exports, "__esModule", { value: true });
